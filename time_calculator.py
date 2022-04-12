f3c1c5b45341e571134aa6958c9d14540ffd4ce3
Tarufetti <91426303+Tarufetti@users.noreply.github.com>
2022-04-12 16:59:28 -0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,18 +1,13 @@
-
-STRINGS DO NOT CHANGE!!!
-
-
-
 def time_calculator(start, duration, day='none'):
 
-    start = '12:00 PM'
-    duration = '5:00'
+
     day=['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
     n_days_later = 0
 
-    starting_hour= int(start[0:2])
-    minutes=int(start[3:5])
-    am_pm= start[6:]
+    starting_colon = start.find(':')
+    starting_hour= int(start[:starting_colon])
+    minutes=int(start[starting_colon+1:starting_colon+3])
+    am_pm= start[starting_colon+4:]
 
     duration_colon = duration.find(':')
     duration_hour = int(duration[:duration_colon])
@@ -33,6 +28,6 @@ def time_calculator(start, duration, day='none'):
         else:
             minutes = minutes + duration_minutes
     
-    return duration_hour
-print (time_calculator('12:00 AM', '24:00'))
+    return n_days_later
+print (time_calculator('1:00 PM', '24:00'))
         
