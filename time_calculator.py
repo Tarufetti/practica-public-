c437fc3f01498d62e963092c31b514b1d70946dd
Tarufetti <91426303+Tarufetti@users.noreply.github.com>
2022-04-13 18:22:58 -0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,8 +1,10 @@
 def time_calculator(start, duration, day='none'):
 
 
-    day=['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+    days={'monday':1,'tuesday':2,'wednesday':3,'thursday':4,'friday':5,'saturday':6,'sunday':7}
+    days_backwards={1:'monday',2:'tuesday',3:'wednesday',4:'thursday',5:'friday',6:'saturday',7:'sunday'}
     n_days_later = 0
+    final_day = ''
 
     starting_colon = start.find(':')
     starting_hour= int(start[:starting_colon])
@@ -15,19 +17,56 @@ def time_calculator(start, duration, day='none'):
 
     if duration_hour >=24:
         n_days_later += int(duration_hour/24)
-        duration_hour = duration_hour%24
-            
+        duration_hour = duration_hour%24            
 
     if am_pm == 'AM':
         if minutes + duration_minutes >= 60:
             starting_hour += 1
-            minutes = (minutes + duration_minutes) -60
-            if starting_hour == 13:
-                starting_hour = 1
-                am_pm = 'PM'
+            minutes = (minutes + duration_minutes)-60
+            duration_minutes=0
         else:
             minutes = minutes + duration_minutes
-    
-    return n_days_later
-print (time_calculator('1:00 PM', '24:00'))
-        
+            duration_minutes=0
+        starting_hour += duration_hour
+        duration_hour=0
+        if starting_hour >= 12:
+                am_pm = 'PM'
+        if starting_hour >= 13:
+            starting_hour = starting_hour-12
+
+    if am_pm == 'PM':
+        if minutes + duration_minutes >= 60:
+            starting_hour += 1
+            minutes = (minutes + duration_minutes)-60
+            duration_minutes=0
+        else:
+            minutes = minutes + duration_minutes
+            duration_minutes=0
+        starting_hour += duration_hour
+        duration_hour=0  
+        if starting_hour >= 12:
+            am_pm = 'AM' 
+            n_days_later +=1  
+        if starting_hour >= 13:
+            starting_hour = starting_hour-12
+    if am_pm == 'AM':
+        if starting_hour > 12:
+                am_pm = 'PM'
+        if starting_hour >= 13:
+            starting_hour = starting_hour-12
+
+
+
+
+
+
+
+    if day in days:
+        if days[day] + n_days_later > 7:
+            final_day = days_backwards[((days[day]+n_days_later)-7)]
+        else:
+            final_day = days_backwards[(days[day]+n_days_later)]
+
+
+    return starting_hour, minutes, am_pm, final_day, n_days_later
+print (time_calculator('11:21 AM', '00:40', 'monday'))
