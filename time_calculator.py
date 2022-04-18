8e7768e636a8179159488f26de92d85588cbe95a
Tarufetti <91426303+Tarufetti@users.noreply.github.com>
2022-04-18 18:39:55 -0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,8 +1,8 @@
 def time_calculator(start, duration, day='none'):
 
 
-    days={'monday':1,'tuesday':2,'wednesday':3,'thursday':4,'friday':5,'saturday':6,'sunday':7}
-    days_backwards={1:'monday',2:'tuesday',3:'wednesday',4:'thursday',5:'friday',6:'saturday',7:'sunday'}
+    days={'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
+    days_index={1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday',7:'Sunday'}
     n_days_later = 0
     final_day = ''
 
@@ -10,6 +10,7 @@ def time_calculator(start, duration, day='none'):
     starting_hour= int(start[:starting_colon])
     minutes=int(start[starting_colon+1:starting_colon+3])
     am_pm= start[starting_colon+4:]
+    final_ampm=''
 
     duration_colon = duration.find(':')
     duration_hour = int(duration[:duration_colon])
@@ -17,7 +18,9 @@ def time_calculator(start, duration, day='none'):
 
     if duration_hour >=24:
         n_days_later += int(duration_hour/24)
-        duration_hour = duration_hour%24            
+        duration_hour = duration_hour%24
+
+        
 
     if am_pm == 'AM':
         if minutes + duration_minutes >= 60:
@@ -29,11 +32,21 @@ def time_calculator(start, duration, day='none'):
             duration_minutes=0
         starting_hour += duration_hour
         duration_hour=0
-        if starting_hour >= 12:
-                am_pm = 'PM'
-        if starting_hour >= 13:
-            starting_hour = starting_hour-12
 
+        if starting_hour == 24:
+            starting_hour -=12
+            n_days_later +=1
+            final_ampm = 'AM'
+        elif starting_hour > 24:
+            starting_hour -=24
+            n_days_later +=1
+            final_ampm = 'AM'
+        elif starting_hour >= 12:
+                final_ampm = 'PM'
+        elif starting_hour >= 13:
+            starting_hour = starting_hour-12
+  #EL ERROR SEGURAMENTE ESTA ACA, PERO NO LO VEO. SOLO PASA EN PM CON MAS DE 24 HORAS DE DURACION HASTA 47. SI FUNCIONA EN 36, PERO NO EN EL RESTO.
+  #A PARTIR DE LOS 49 VUELVE A FUNCIONAR. 
     if am_pm == 'PM':
         if minutes + duration_minutes >= 60:
             starting_hour += 1
@@ -43,30 +56,47 @@ def time_calculator(start, duration, day='none'):
             minutes = minutes + duration_minutes
             duration_minutes=0
         starting_hour += duration_hour
-        duration_hour=0  
-        if starting_hour >= 12:
-            am_pm = 'AM' 
-            n_days_later +=1  
-        if starting_hour >= 13:
-            starting_hour = starting_hour-12
-    if am_pm == 'AM':
-        if starting_hour > 12:
-                am_pm = 'PM'
-        if starting_hour >= 13:
-            starting_hour = starting_hour-12
+        duration_hour=0
+        if starting_hour >=12 and starting_hour <24:
+            n_days_later +=1
+            final_ampm = 'AM'
+            if starting_hour >= 13 and starting_hour <24:
+                starting_hour = starting_hour-12
+        if starting_hour == 24:
+            starting_hour -=12
+            final_ampm = 'PM'
+        elif starting_hour >= 25:
+            starting_hour -=24
+            n_days_later +=1
+            final_ampm = 'PM'
 
+            
+        
+    
+    if len(str(minutes)) == 1:
+        minutes= f'0{minutes}'
+    final_hour= f'{starting_hour}{start[starting_colon]}{minutes} {final_ampm}'
+    print(final_hour)
+    
 
-
-
-
-
-
+    day = day.capitalize()
     if day in days:
         if days[day] + n_days_later > 7:
-            final_day = days_backwards[((days[day]+n_days_later)-7)]
+            final_day = days_index[((days[day]+n_days_later)-7)]
         else:
-            final_day = days_backwards[(days[day]+n_days_later)]
+            final_day = days_index[(days[day]+n_days_later)]
+        if n_days_later == 0:
+            return f'{final_hour}, {final_day}'
+        elif n_days_later == 1:
+            return f'{final_hour}, {final_day} (next day)'
+        elif n_days_later > 2:
+            return f'{final_hour}, {final_day} ({n_days_later} days later)'
+    else:
+        if n_days_later == 0:
+            return f'{final_hour}'
+        elif n_days_later == 1:
+            return f'{final_hour} (next day)'
+        elif n_days_later > 2:
+            return f'{final_hour}, {final_day} ({n_days_later} days later)'
 
-
-    return starting_hour, minutes, am_pm, final_day, n_days_later
-print (time_calculator('11:21 AM', '00:40', 'monday'))
+print(time_calculator('11:21 PM', '47:40', 'monday'))
